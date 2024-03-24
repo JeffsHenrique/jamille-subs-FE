bf37b0234f0fed35ee86b2598b21b15b8455e415
JeffsHenrique <henrique.cps15@gmail.com>
2024-03-24 13:33:09 -0300

    removing unecessary service

--- src/services/VideoControlService.ts
+++
@@ -1,9 +0,0 @@
-class VideoControlService {
-    private videoElement: HTMLVideoElement | null = null
-
-    initialize(videoElement: HTMLVideoElement) {
-        this.videoElement = videoElement
-    }
-}
-
-export default VideoControlService
