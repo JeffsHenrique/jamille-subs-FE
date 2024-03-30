eccce5464ae23df849b967930a203609246e4509
JeffsHenrique <henrique.cps15@gmail.com>
2024-03-29 21:18:28 -0300

    Implementing booster audio

--- src/helpers/formatTime.ts
+++ src/helpers/formatTime.ts
@@ -1,7 +1,14 @@
-export const formatTime = (time: number) => {
+export const formatTimeComplete = (time: number) => {
     const minutes = Math.floor(time / 60)
     const seconds = Math.floor(time % 60)
     const milliseconds = Math.floor((time - Math.floor(time)) * 1000)
 
     return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')},${String(milliseconds).padStart(3, '0')}`
+}
+
+export const formatTimeWithoutMilliseconds = (time: number) => {
+    const minutes = Math.floor(time / 60)
+    const seconds = Math.floor(time % 60)
+
+    return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`
 }
