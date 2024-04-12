ac57376fbca87571f219dad39e3351b3ad801d96
JeffsHenrique <henrique.cps15@gmail.com>
2024-04-12 17:59:15 -0300

    Changing download .txt file name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,23 @@ import { VideoPlayer } from "@/components/VideoPlayer"
 import { TextField } from "@/components/TextField"
 import { ThemeProvider } from "@/contexts/ThemeContext"
 import { Footer } from "@/components/Footer"
+import { useState } from "react"
 
 const Page = () => {
+  const [fileName, setFileName] = useState<string | null>(null)
+
+  const handleFileName = (fileName: string) => {
+    setFileName(fileName.replace('.mp4', '.txt'))
+  }
+
   return (
     <ThemeProvider>
       <div className="w-full h-[calc(100vh-8vh)] bg-slate-300 dark:bg-gray-950">
         <Header />
 
         <div className="flex flex-row mt-2 gap-8">
-          <VideoPlayer />
-          <TextField />
+          <VideoPlayer onFileSelect={handleFileName} />
+          <TextField fileName={fileName} />
         </div>
 
       </div>
